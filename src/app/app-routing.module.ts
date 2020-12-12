37f912b9f37e82304a68de346e577b3408105ead
Sameera Indika <sameeraindikaN@gmail.com>
2020-12-12 17:00:35 +0530

    recovered all codes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { MenuComponent } from './menu/menu.component';
-import { NewPatientRegistrationComponent } from './new-patient-registration/new-patient-registration.component';
 import { ViewPatientDetailsComponent } from './view-patient-details/view-patient-details.component';
+import { NewPatientRegistrationComponent } from './new-patient-registration/new-patient-registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Welcome', pathMatch: 'full' },
   { path: 'Welcome', component: LoginFormComponent },
   { path: 'Menu', component: MenuComponent },
   { path: 'Admin',  component: AdminPageComponent },
-  { path: 'NewPatient', component: NewPatientRegistrationComponent },
-  { path: 'PatientDetails', component: ViewPatientDetailsComponent }
+  { path: 'PatientDetails', component: ViewPatientDetailsComponent },
+  { path: 'NewPatient', component: NewPatientRegistrationComponent }
 ];
 
 @NgModule({
